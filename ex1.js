c39a5aa8a475c33a430cf03d1a352f6245ab377b
Ana Beatriz <anabemorita@gmail.com>
2018-06-16 00:43:53 -0300

    Finalizar ex1

--- ex1.js
+++ ex1.js
@@ -6,10 +6,75 @@ cliente.salario = Number(prompt("Qual o seu salario?"));
 
 let emprestar = false;
 
+/*
+//Modo um: três blocos de if separados para verificar cada condição separadamente 
+//e um if final para ver se as 3 são satisfeitas 
 if(cliente.idade > 21 && cliente.idade < 56){
     emprestar = true;
 }
 
 if(cliente.valor >= 1000){
     emprestar = true;
+} else{
+    emprestar = false;
+  }
+
+if(cliente.valor < (cliente.salario * 15)){
+    emprestar = true;
+} else{
+    emprestar = false;
+  }
+
+if(emprestar){
+    alert("ACEITO");
+} else{
+    alert("NÃO ACEITO");
+  }
+*/
+
+/*
+//Modo dois: if encadeado
+//se alguma condição não for satisfeita emprestar é falso
+if(cliente.idade > 21 && cliente.idade < 56){
+    emprestar = true;
+    if(cliente.valor >= 1000){
+        emprestar = true;
+        if(cliente.valor < (cliente.salario * 15)){
+            emprestar = true;
+            alert("ACEITO");//só emite o alert se as três condições forem verdadeiras
+        }
+        else{
+            emprestar = false;
+        }
+    } else{
+        emprestar = false;
+      }    
+}
+if(!emprestar){
+    alert("NÃO ACEITO");    
+}
+*/
+
+//modo simplificado
+//esse bloco de if verifica se todas as três condições de empréstimo são satisfeitas ao mesmo tempo
+if((cliente.idade > 21 && cliente.idade < 55) && cliente.valor > 1000 && (cliente.valor < (cliente.salario * 15))){
+    emprestar = true;
+}
+
+//EXTRA
+if(emprestar){
+    //cria a variavel prestacao e recebe valor já convertido para numero  
+    let parcelas = Number(prompt("Em quantas prestações pretende pagar?"));
+    let montante;
+
+    if(prestacao > 2 && prestacao < 21){
+        montante = cliente.valor * (1 + 0.08^prestacao);
+    }
+
+    let prestacao = montante / parcelas;
+    alert("ACEITO");
+    alert(`O valor total do empréstimo é de R$${montante} reais, a serem pagos em ${parcelas} parcelas de R$${prestacao}`);
+}
+else{
+    alert("NÃO ACEITO");
 }
