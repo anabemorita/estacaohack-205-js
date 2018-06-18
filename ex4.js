670e16e1af93ec71cc2d7c3fe90115054cb9a541
Ana Beatriz <anabemorita@gmail.com>
2018-06-18 01:00:43 -0300

    Alterar ordem de exibição da lista

--- ex4.js
+++ ex4.js
@@ -1,13 +1,14 @@
 let meninos = ["João", "Lucas", "Mateus", "Pedro", "Rodrigo"];
 let meninas = ["Carol", "Fernanda", "Gabriela", "Isabela", "Maria"];
-let duplas = [];
+let duplas = {};
 let contador = 0;
 let n = 0;
 
+// alteração lista
 while(contador < meninos.length){
     let i = 0;
     while(i < meninas.length) {
-        duplas[n] = meninos[contador] + " e " + meninas[i];
+        duplas[n] = meninos[i] + " e " + meninas[i];
         i++;
         n++;
     }
