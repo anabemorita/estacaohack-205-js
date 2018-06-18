e52964dfc1f7c458cdb4bd63a9e6e8514ce14c49
Ana Beatriz <anabemorita@gmail.com>
2018-06-17 22:35:18 -0300

    Corrigir fórmula de juros compostos

--- ex1.js
+++ ex1.js
@@ -47,9 +47,9 @@ if(cliente.idade > 21 && cliente.idade < 56){
             emprestar = false;
         }
     } else{
-        emprestar = false;
+        emprestar = false;s
       }    
-}
+}s
 if(!emprestar){
     alert("NÃO ACEITO");    
 }
@@ -57,7 +57,7 @@ if(!emprestar){
 
 //modo simplificado
 //esse bloco de if verifica se todas as três condições de empréstimo são satisfeitas ao mesmo tempo
-if((cliente.idade > 21 && cliente.idade < 55) && cliente.valor > 1000 && (cliente.valor < (cliente.salario * 15))){
+if((cliente.idade > 21 && cliente.idade < 55) && cliente.valor >= 1000 && (cliente.valor < (cliente.salario * 15))){
     emprestar = true;
 }
 
@@ -68,7 +68,7 @@ if(emprestar){
     let montante;
 
     if(prestacao > 2 && prestacao < 21){
-        montante = cliente.valor * (1 + 0.08^prestacao);
+        montante = cliente.valor * Math.pow(1 + 0.08, prestacao);
     }
 
     let prestacao = montante / parcelas;
